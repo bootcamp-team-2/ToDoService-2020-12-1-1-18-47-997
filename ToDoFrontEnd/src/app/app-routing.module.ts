4421de29ea9fb698d76949fcd5df4491817f5cf5
YYuan1414 <yuanyiren19900728@163.com>
2020-12-01 11:25:38 +0800

    feat: add router to detail, create and edit

--- ToDoFrontEnd/src/app/app-routing.module.ts
+++ ToDoFrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { ListTodoitemComponent } from './list-todoitem/list-todoitem.component';
+import { TodoitemDetailComponent } from './todoitem-detail/todoitem-detail.component';
+import { CreateTodoitemComponent } from './create-todoitem/create-todoitem.component';
+import { UpdateTodoItemComponent } from './update-todo-item/update-todo-item.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: "", component: ListTodoitemComponent},
+  {path: "detail", component: TodoitemDetailComponent},
+  {path: "create", component: CreateTodoitemComponent},
+  {path: "edit", component: UpdateTodoItemComponent}
+];
+// 进行页面的切换
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
