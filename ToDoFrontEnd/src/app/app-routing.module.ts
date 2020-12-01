e3ec6f8dc034ea2829b4f11386c316aa73f19777
Lei ZHU <lzhu19@slb.com>
2020-12-01 11:25:30 +0800

    feat:use routing when click button

--- ToDoFrontEnd/src/app/app-routing.module.ts
+++ ToDoFrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { ListTodoitemComponent } from './list-todoitem/list-todoitem.component';
+import { CreateTodoitemComponent } from './create-todoitem/create-todoitem.component';
+import { TodoitemDetailComponent } from './todoitem-detail/todoitem-detail.component';
+import { UpdateTodoItemComponent } from './update-todo-item/update-todo-item.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: ListTodoitemComponent},
+  {path: 'creat', component: CreateTodoitemComponent},
+  {path: 'edit', component: UpdateTodoItemComponent},
+  {path: 'detail', component: TodoitemDetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
