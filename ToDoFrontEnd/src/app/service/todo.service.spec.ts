72d4a8c50ee1651e65181c472be910544d8679f7
Lei ZHU <lzhu19@slb.com>
2020-12-01 16:27:58 +0800

    can create via mockhttp

--- ToDoFrontEnd/src/app/service/todo.service.spec.ts
+++ ToDoFrontEnd/src/app/service/todo.service.spec.ts
@@ -1,42 +1,90 @@
+import { TodoHttpService } from './todo-http.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { defer, of } from 'rxjs';
 import { ToDoItem } from '../model/ToDoItem';
 import { TodoStoreService } from './todo-store.service';
 import { TodoService } from './todo.service';
+// import{ TodoHttpService } from './todo-http.service';
 
 describe('TodoService', () => {
 
   let service: TodoService;
-  let httpClientSpy: { get: jasmine.Spy };
+  let httpClientSpy: { get: jasmine.Spy, post: jasmine.Spy };
   let todoStoreService: TodoStoreService;
+  let todoHttpService: TodoHttpService;
+
 
   beforeEach(() => {
-    // TODO: spy on other methods too
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put']);
     todoStoreService = new TodoStoreService();
+    todoHttpService = new TodoHttpService(<any>httpClientSpy);
 
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TodoService);
+    service = new TodoService(todoStoreService, todoHttpService);
   });
+
+  function asyncData<T>(data: T) {
+    return defer(() => Promise.resolve(data));
+  }
+  
+  function asyncError(errorObject: any) {
+      return defer(() => Promise.reject(errorObject));
+    }
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
   it('should get all todoitems', () => {
+    const expectAllTodoItems = todoStoreService.GetAll();
+    httpClientSpy.get.and.returnValue(of(expectAllTodoItems));
     expect(service.todoItems.length).toBe(5);
+    expect(httpClientSpy.get.calls.count()).toBe(1,"one call");
   });
+
+  it('should process error response when get all todoItems fail', fakeAsync( () => {
+
+    //given
+    const errorResponse = new HttpErrorResponse({
+      error: 'test 404 error',
+      status: 404, statusText: 'Not Found'
+    });
+    httpClientSpy.get.and.returnValue(asyncError(errorResponse));
+    //when
+    service.todoItems;
+    tick(50);
+    //then
+    expect(service.getAllFailMessage).toBe('Get all fail because web API error');
+  }));
+
+
 
   it('should create todo-item via mockhttp', () => {
     const newTodoItem = new ToDoItem(10, "new todo", "new todo description", false);
+    httpClientSpy.post.and.returnValue(of(newTodoItem));
+
     service.Create(newTodoItem);
-    expect(service.todoItems.length).toBe(6);
-    expect(service.todoItems[5].id === newTodoItem.id);
-    expect(service.todoItems[5].title === newTodoItem.title);
-    expect(service.todoItems[5].description === newTodoItem.description);
-    expect(service.todoItems[5].isDone === newTodoItem.isDone);
+    //then
+    expect(httpClientSpy.post.calls.count()).toBe(1,"one call");
   });
+
+  it('should process error response when create todoItems fail', fakeAsync( () => {
+
+    //given
+    const errorResponse = new HttpErrorResponse({
+      error: 'test 404 error',
+      status: 404, statusText: 'Not Found'
+    });
+    const newTodoItem = new ToDoItem(10, "new todo", "new todo description", false);
+    httpClientSpy.post.and.returnValue(asyncError(errorResponse));
+    //when
+    service.Create(newTodoItem);
+    tick(50);
+    //then
+    expect(service.postFailMessage).toBe('Post fail because web API error');
+  }));
+
+
 
   it('should update todo-item', () => {
     const updateTodoItem = service.todoItems[0];
--- ToDoFrontEnd/src/app/service/todo.service.ts
+++ ToDoFrontEnd/src/app/service/todo.service.ts
@@ -1,3 +1,4 @@
+import { TodoHttpService } from './todo-http.service';
 import { Injectable } from '@angular/core';
 import { ToDoItem } from '../model/ToDoItem';
 import { TodoStoreService } from './todo-store.service';
@@ -6,45 +7,63 @@ import { TodoStoreService } from './todo-store.service';
   providedIn: 'root'
 })
 export class TodoService {
-  
+
   public updatingToDoItem: ToDoItem;
   public selectedTodoItem: ToDoItem;
   private currentId: number = 0;
+  public getAllFailMessage: string;
+  public postFailMessage: string;
 
   private _todoItems: Array<ToDoItem>;
 
-  constructor(private todoStore: TodoStoreService) {
+  constructor(private todoStore: TodoStoreService, private todoHttpService: TodoHttpService) {
     this._todoItems = todoStore.GetAll();
     this.updatingToDoItem = new ToDoItem(-1, "", "", false);
     this.selectedTodoItem = new ToDoItem(-1, "", "", false);
-    this.currentId = this.todoItems.length;
+    //this.currentId = this.todoItems.length;
+    this.getAllFailMessage = '';
+    this.postFailMessage = '';
   }
 
   public get todoItems(): Array<ToDoItem> {
-    return this.todoStore.GetAll();
+    const allTodoItem = new Array<ToDoItem>();
+    this.todoHttpService.GetAll().subscribe(todoItems => {
+      allTodoItem.push(...todoItems);
+    },
+    error => {
+      this.getAllFailMessage = 'Get all fail because web API error';
+    }
+    );
+    return allTodoItem;
   }
 
   public SetUpdatingTodoItemId(id: number): void {
     const foundTodoItem = this.todoStore.FindById(id);
-    
+
     if (foundTodoItem !== undefined) {
       this.updatingToDoItem = Object.assign({}, foundTodoItem);
     }
   }
 
   public Create(todoItem: ToDoItem) {
-    todoItem.id = this.currentId;
-    var newTodoItem = Object.assign({}, todoItem);
-    this.todoStore.Create(newTodoItem);
-    this.currentId++;
+    // todoItem.id = this.currentId;
+    // var newTodoItem = Object.assign({}, todoItem);
+    // this.todoStore.Create(newTodoItem);
+    // this.currentId++;
+
+    this.todoHttpService.Create(todoItem).subscribe(todoItem => {console.log(todoItem); this.postFailMessage = ''; },
+    error => {
+      this.postFailMessage = 'Post fail because web API error';
+    });
+
   }
 
   public UpdateTodoItem(updateTodoItems: ToDoItem): void {
     this.todoStore.Update(updateTodoItems);
   }
 
-  public DeleteTodoItem(id: number):void{   
-    this.todoStore.Delete(id); 
+  public DeleteTodoItem(id: number):void{
+    this.todoStore.Delete(id);
   }
 
   public SetSelectedTodoItemId(id: number):void{
