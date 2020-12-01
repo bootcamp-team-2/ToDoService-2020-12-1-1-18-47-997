6906e387b80d8a12a0a466eb203ca4e327d91e19
Lin LIU <lliu41@slb.com>
2020-12-01 11:24:50 +0800

    feat: add routes for components

--- ToDoFrontEnd/src/app/app-routing.module.ts
+++ ToDoFrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateTodoitemComponent } from './create-todoitem/create-todoitem.component';
+import { ListTodoitemComponent } from './list-todoitem/list-todoitem.component';
+import { TodoitemDetailComponent } from './todoitem-detail/todoitem-detail.component';
+import { UpdateTodoItemComponent } from './update-todo-item/update-todo-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListTodoitemComponent },
+  { path: 'create', component: CreateTodoitemComponent },
+  { path: 'detail', component: TodoitemDetailComponent },
+  { path: 'edit', component: UpdateTodoItemComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
